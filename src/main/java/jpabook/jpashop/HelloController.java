c4dc92d131837a05d9400a969ea7eec145d6bec7
junho <jh102328@khu.ac.kr>
2024-02-28 00:22:50 +0900

    second commit

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -7,9 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/1")
     public String hello(Model model){
         model.addAttribute("message", "qwew msg");
         return "hello";
     }
+
+    @GetMapping("/2")
+    public String hello2(Model model){
+        model.addAttribute("message", "qwew msg");
+        return "hello";
+    }
+
+
 }
